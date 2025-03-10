edc5e6a15f100110f8b4bf61adb431c406cbbdf2
DevynKore <devynkorber@gmail.com>
2025-03-10 15:32:13 -0600

    Linear Probing

--- LinearProbing.java
+++ LinearProbing.java
@@ -1,3 +1,20 @@
-public class LinearProbing {
-    
+public class LinearProbing<T> extends Hashtable<T> {
+
+    /**
+     * Constructor: Initializes a hash table with the given size using linear probing.
+     */
+    public LinearProbing(int size) {
+        super(size);
+    }
+
+    /**
+     * Computes the probe index using linear probing.
+     * Formula: h1(key) = key.hashCode() % tableSize
+     * Linear probing: next index = (index + 1) % tableSize
+     */
+    @Override
+    protected int getProbeIndex(T key, int probeCount) {
+        int hash = positiveMod(key.hashCode(), tableSize);
+        return (hash + probeCount) % tableSize; // Linear probing logic
+    }
 }
